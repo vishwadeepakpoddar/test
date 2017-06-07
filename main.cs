6b362f13906f6f975275aa43a705ecc0aee75a00
vish <vish@dis.com>
2017-06-07 20:02:35 +0530

    Giving a personalized hello.

--- main.cs
+++ main.cs
@@ -4,7 +4,19 @@ public static class MyClass
 {
     public static void Main(){
 
-        write
+
+string someName = "Santa"
+        Console.WriteLine("Enter your name.");
+        String myName = Console.ReadLine();
+        if (MyName=="")
+        {
+        Console.WriteLine("hello {0}", someName);
+        
+        }
+        Console.WriteLine("hello {0}", myName);
+
+
+    
     }
 
 
