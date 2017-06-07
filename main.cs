16fc30d7ec1d34486041bfb5579800dfa7b43608
vish <vish@dis.com>
2017-06-07 20:23:44 +0530

    Promt user for last name as well.
    Signed-off-by: dish <dish@dis.com>

--- main.cs
+++ main.cs
@@ -5,15 +5,23 @@ public static class MyClass
     public static void Main(){
 
 
+        Console.WriteLine("Hello World");
 string someName = "Santa"
-        Console.WriteLine("Enter your name.");
+        Console.WriteLine("Enter your first name.");
         String myName = Console.ReadLine();
+        Console.WriteLine("Enter your last name");
+        String lastName = Console.ReadLine();
         if (MyName=="")
         {
         Console.WriteLine("hello {0}", someName);
         
         }
+        if (lastName == "")
+        {
+        
         Console.WriteLine("hello {0}", myName);
+        }
+        Console.WriteLine("hello {0} {1}", myName, lastName);
 
 
     
